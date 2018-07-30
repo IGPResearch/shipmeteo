3dde982f0e035e12f47ce69716716ba1d3c707d4
Dennis Sergeev <dennis.sergeev@gmail.com>
2018-07-30 18:49:14 +0100

    add mypaths and NB to explore weatherpack data

---
+++ mypaths.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Paths to data
+"""
+import os
+from pathlib import Path
+
+# Root of the current repository
+curdir = Path('.').absolute().parent
+sample_dir = curdir/'data'
+
+# External data directories
+igp_data_dir = Path('/media')/os.getenv('USER')/'Elements'/'IGP'/'data'
+wpk_dir = igp_data_dir/'weatherpacks'
+nmea_dir = igp_data_dir/'nmea_logs'
+masin_dir = igp_data_dir/'masin'
+dundee_dir = igp_data_dir/'dundee'
+ostia_dir = igp_data_dir/'ostia'
+amsr2_dir = igp_data_dir/'amsr2'
+
+# Output directories
+plotdir = curdir/'figures'
